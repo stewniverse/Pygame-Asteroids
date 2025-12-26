9050d6cf83e42cb3f5f6bed762da63191f368db8
Kadesh Stewart <kks9788@nyu.edu>
2025-12-25 20:39:34 -0500

    setting up game loop, setting up game logic, setting up game quit criteria

--- main.py
+++ main.py
@@ -4,13 +4,24 @@
 import pygame
 from database import connect_database, database_version
 from constants import *  
+from logger import log_state
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    while (True):
+        log_state()
+        # check to see if the user has closed the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
