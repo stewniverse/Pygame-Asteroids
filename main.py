f1b580fcac2b783a50231c9411218f43f08ded3c
Kadesh Stewart <kks9788@nyu.edu>
2025-12-29 23:04:14 -0500

    add the player

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 # and the database_version variable
 # from database.py into the current file
 import pygame
+from player import Player
 from database import connect_database, database_version
 from constants import *  
 from logger import log_state
+
 
 def main():
     pygame.init()
@@ -12,6 +14,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # starting positions 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    p1 = Player(x, y)
 
     # create a new game clock object
     clock = pygame.time.Clock()
@@ -27,8 +34,10 @@ def main():
                 return
         
         screen.fill("black")
-        pygame.display.flip()
+        p1.update(dt)
+        p1.draw(screen)
         dt = clock.tick(60)/1000
+        pygame.display.flip()
         # print(dt)
 
 if __name__ == "__main__":
